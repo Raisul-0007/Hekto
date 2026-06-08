f897aca561062f6771e2ef4ad8f67d35d643c3bf
Raisul-0007 <raisulislamriat007@gmail.com>
2026-06-08 16:29:50 +0600

    Best complete

--- src/components/Best.jsx
+++ src/components/Best.jsx
@@ -1,19 +1,34 @@
 import react,{useContext} from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import {Data} from "./ApiData"
 import Container from './Container'
+import SliderImport from "react-slick";
+
+const Slider = SliderImport.default;
 const Best = () => {
     let {info} = useContext(Data)
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+      
   return (
-    <div>
+    <div className="py-20">
       <Container>
         <div className="">
             <h2 className="text-4xl text-center">Best Selling Products</h2>
         </div>
         <div className="">
-            {info.map((item)=>(
-                <div className="w-1/4">
+            <Slider className="py-15" {...settings}>
+            {info.slice(121, 141 ).map((item)=>(
+                <div className="w-1/4 px-4" key={item.id}>
                     <div className="">
-                        <img className="w-full object-cover rounded-full shadow-xl bg-[#f6f7fb] p-5" src={item.thumbnail} alt={item.id} />
+                        <img className=" object-cover rounded-full shadow-xl bg-[#f6f7fb] p-5" src={item.thumbnail} alt={item.id} />
                     </div>
                     <div className="">
                         <h4 className="text-xl font-bold text-[#151875] text-center">{item.title.slice(0, 13)}</h4>
@@ -24,6 +39,7 @@ const Best = () => {
                     </div>
                 </div>
             ))}
+            </Slider>
         </div>
       </Container>
     </div>
